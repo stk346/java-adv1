d955592eaeb95767a7d3bd31f391b9c5df746f9f
stk346 <stk346@gamil.com>
2024-08-16 13:32:32 +0900

    feat: commit

---
+++ src/thread/control/CheckedExceptionMain.java
@@ -0,0 +1,19 @@
+package thread.control;
+
+import util.ThreadUtils;
+
+import static util.ThreadUtils.*;
+
+public class CheckedExceptionMain {
+    public static void main(String[] args) throws Exception {
+        throw new Exception();
+    }
+
+    static class CheckedRunnable implements Runnable {
+
+        @Override
+        public void run() {
+            sleep(1000);
+        }
+    }
+}
